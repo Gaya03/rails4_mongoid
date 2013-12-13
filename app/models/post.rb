a8b300b92664185b8fff3cfbd029c14b44711200
sashokcs <sashokkumarit@gmail.com>
2013-12-13 15:25:31 +0530

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :title, type: String
+  field :body, type: String
+  field :starred, type: Mongoid::Boolean
+  index({ starred: 1 })
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4Mongoid::Application.config.secret_key_base = '1924f28b450ab1aad1814959189fb1e735288b4a95c89906514b4278711d777bb5aed20db3ece9420feb3687130cc451c406a0fc1efd9ec5065a97d21846c1d7'
